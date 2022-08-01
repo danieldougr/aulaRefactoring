250aa87cfe22b027582a7c39b8ee14e43b0ce7f2
danieldougr <danieldougr@gmail.com>
2022-08-01 09:54:57 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -6,11 +6,14 @@ public class HtmlStatement extends Statement {
         Enumeration rentals = aCustomer.getRentals();
         String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
         "</EM></H1><P>\n";
+        result = "Rental Record for " + aCustomer.getName()+" ";
         while (rentals.hasMoreElements()) {
            Rental each = (Rental) rentals.nextElement();
            //show figures for each rental
            result += each.getMovie().getTitle()+ ": " +
            String.valueOf(each.getCharge()) + "<BR>\n";
+           //show figures for rental
+           result += each.getMovie().getTitle()+ ": " + String.valueOf(each.getCharge())+ " ";
         }
         //add footer lines
         result += "<P>You owe <EM>" +
@@ -18,6 +21,8 @@ public class HtmlStatement extends Statement {
         result += "On this rental you earned <EM>" + 
         String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
         "</EM> frequent renter points<P>";
+        result += "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + " ";
+        result += "You earned " + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
      }
 
